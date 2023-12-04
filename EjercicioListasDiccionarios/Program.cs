5042fab74f8c821d6ce1f64008275fbd9a2e65e8
AlbaZ <albazuniga2202@gmail.com>
2023-12-04 10:54:55 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioListasDiccionarios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioListasDiccionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> productos = new List<string>();
+            productos.Add("Acondicionador");
+            productos.Add("Shampoo");
+            productos.Add("Jabon");
+            productos.Insert(3, "Nuevo Producto");
+            productos.Add("Pasta");
+            productos.Add("Ace");
+            productos.Add("Cloro");
+            productos.Add("Aceite");
+            productos.Add("Acido");
+            productos.Add("Bicarbonato");
+            productos.RemoveAt(productos.Count - 1);
+            foreach (string producto in productos)
+            {
+                Console.WriteLine(producto);
+            }
+
+
+        }
+    }
+}
